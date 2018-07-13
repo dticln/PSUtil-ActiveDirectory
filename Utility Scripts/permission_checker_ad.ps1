c5c14a66dd9775b9cea52ed61b2e971e9d39f1f6
MARCELO GOMES MARTINS_Admin <00271922-admin@ad.ufrgs.br>
2018-07-13 10:04:41 -0300

    Permission Checker Update

--- Utility Scripts/permission_checker_ad.ps1
+++ Utility Scripts/permission_checker_ad.ps1
@@ -1,21 +1,39 @@
-﻿Function listaDiretorios {
-    $lista = Get-ChildItem '\\ad.ufrgs.br\LITORAL' | where-object {($_.PsIsContainer)} | Get-ACL
-    $array = @()
-    listaDiretorioRecursivo($lista, '\\ad.ufrgs.br\LITORAL', $array)
-}
-
-Function listaDiretorioRecursivo {
-    Param($lista, $caminho, $array)
-    $caminho = $lista.Path
-    Write-Output $caminho
-    ForEach($item in $caminho) {
-        if ($caminho) {
-            Write-Output $path.Substring( $path.LastIndexOf('\')[0])
+﻿$directories = Get-ChildItem '\\ad.ufrgs.br\LITORAL' -Directory -ErrorAction SilentlyContinue -Recurse
+$array = @{}
+Foreach ($directory In $directories.FullName) {
+    $acl = Get-ChildItem $directory -ErrorAction SilentlyContinue | where-object {($_.PsIsContainer)} | Get-ACL -ErrorAction SilentlyContinue
+    If ($acl) {
+        Foreach($idRef In $acl.Access.IdentityReference){
+            If ($idRef.Value -match 'AD\\[0-9]{8}' -and $acl.Owner -notcontains $idRef.Value) {
+                If ($array.Contains($idRef.Value)){
+                    If ($array[$idRef.Value] -notcontains $directory) {
+                        $array[$idRef.Value] += $directory
+                        $name = $directory.Substring($directory.LastIndexOf("\"))
+                        Write-Output "Pasta `"...$name`" atribuída à `"$($idRef.Value)`"."
+                    }
+                } Else {
+                    $list = @()
+                    $list += $directory
+                    $array.Add($idRef.Value, $list);
+                    Write-Output "Usuário `"$($idRef.Value)`" possui acesso manual."
+                }
+            }
         }
-        #$childrens = Get-ChildItem "$caminho$pasta" | where-object {($_.PsIsContainer)} | Get-ACL
-        #$array += $childrens
-        #listaDiretorioRecursivo($childrens, "$caminho$pasta", $array)
     }
 }
 
-listaDiretorios
+
+$out = @()
+Foreach ($key In $array.Keys) {
+    $item = $array[$key]
+    $key = $key.Substring($key.Length - 8)
+    Foreach ($sub in $item) {
+        $obj = [PSCustomObject]@{
+            Usuário = $key
+            Local = $sub
+        }
+        $out += $obj
+    }
+}
+
+$out | Export-Csv "output.csv" -Encoding UTF8 -NoTypeInformation
